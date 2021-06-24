f5a17f0c0ba8f04c8c51122b4cca096d1223c39a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-25 00:51:23 +0530

    Add: Test Cases

--- TU_MyQueue.java
+++ TU_MyQueue.java
@@ -19,4 +19,19 @@ public class TU_MyQueue {
         boolean result = myQueue.head.equals(myFirstNode) && myQueue.tail.equals(myThirdNode);
         Assertions.assertTrue(result);
     }
+    //for dequeue
+    @Test
+    void whenDequeueNodeNextToHeadShouldBeHead() {
+
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.dequeue();
+        boolean result = myQueue.head.equals(mySecondNode) && myQueue.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
 }
