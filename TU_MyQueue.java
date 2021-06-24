a35c8888ccd80cc80f20d4f229f85691f3094be6
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-06-25 00:45:56 +0530

    Add: Test Casee

---
+++ TU_MyQueue.java
@@ -0,0 +1,22 @@
+package com.abhi.stack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TU_MyQueue {
+    //for enqueue
+    @Test
+    void whenEnqueueFirstNodeShouldBeHead() {
+
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+
+        boolean result = myQueue.head.equals(myFirstNode) && myQueue.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+}
